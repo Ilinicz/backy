2df119350f7b8143cc02241615f6801b6c887260
Vincent Ilinicz <vilinicz@mail.ru>
2014-05-13 15:34:48 +0400

    Just JSON API

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  before_action :set_article, only: [:show, :update, :destroy]
+
   respond_to :json, :content_type => 'application/json'
+  
   # GET /articles
   # GET /articles.json
   def index
@@ -10,57 +13,26 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
-    respond_with Article.find(params[:id])
+    respond_with @article
   end
 
-#  # GET /articles/new
-#  def new
-#    @article = Article.new
-#  end
-#
-#  # GET /articles/1/edit
-#  def edit
-#  end
-#
-#  # POST /articles
-#  # POST /articles.json
-#  def create
-#    @article = Article.new(article_params)
-#
-#    respond_to do |format|
-#      if @article.save
-#        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-#        format.json { render :show, status: :created, location: @article }
-#      else
-#        format.html { render :new }
-#        format.json { render json: @article.errors, status: :unprocessable_entity }
-#      end
-#    end
-#  end
-#
-#  # PATCH/PUT /articles/1
-#  # PATCH/PUT /articles/1.json
-#  def update
-#    respond_to do |format|
-#      if @article.update(article_params)
-#        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-#        format.json { render :show, status: :ok, location: @article }
-#      else
-#        format.html { render :edit }
-#        format.json { render json: @article.errors, status: :unprocessable_entity }
-#      end
-#    end
-#  end
-#
-#  # DELETE /articles/1
-#  # DELETE /articles/1.json
-#  def destroy
-#    @article.destroy
-#    respond_to do |format|
-#      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-#      format.json { head :no_content }
-#    end
-#  end
+  # POST /articles
+  # POST /articles.json
+  def create
+    respond_with Article.create(article_params)
+  end
+
+  # PATCH/PUT /articles/1
+  # PATCH/PUT /articles/1.json
+  def update
+    respond_with @article.update(article_params)
+  end
+
+  # DELETE /articles/1
+  # DELETE /articles/1.json
+  def destroy
+    respond_with @article.destroy
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
